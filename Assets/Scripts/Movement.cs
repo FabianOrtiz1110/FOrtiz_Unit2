366c5ea5cbbd5924d22d480295f615236d1560e7
FabianOrtiz1110 <112835551+FabianOrtiz1110@users.noreply.github.com>
2023-09-16 22:09:41 -0700

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float HorizontalInput;
+    public float speed = 10.0f;
+    private float xRange = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * HorizontalInput * Time.deltaTime * speed);
+
+        if (transform.position.x > xRange)
+            {
+                transform.position = new Vector3 (xRange, transform.position.y, transform.position.z);
+            }
+
+        if (transform.position.x < -xRange)
+            {
+                transform.position = new Vector3 (-xRange, transform.position.y, transform.position.z);
+            }
+
+        
+    }
+}
