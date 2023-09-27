d7d865e46eba60f683f64b5ad6eae8e65aa83484
FabianOrtiz1110 <112835551+FabianOrtiz1110@users.noreply.github.com>
2023-09-27 01:43:55 -0700

    LessonD
    LessonD

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,23 +6,25 @@ public class SpawnManager : MonoBehaviour
 {
 
 public GameObject [] animalPrefabs;
-private float xPosRange = 13;
+private float xPosRange = 17;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", 3, 1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            float randXPos = Random.Range(-xPosRange, xPosRange);
-            int animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 randPos = new Vector3(randXPos, 0, 21);
-            Instantiate(animalPrefabs[animalPrefabIndex], randPos,
-                animalPrefabs[animalPrefabIndex].transform.rotation);
-        }
+
+    }
+
+    void SpawnRandomAnimal()
+    {
+        float randXPos = Random.Range(-xPosRange, xPosRange);
+        int animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 randPos = new Vector3(randXPos, 0, 21);
+        Instantiate(animalPrefabs[animalPrefabIndex], randPos,
+            animalPrefabs[animalPrefabIndex].transform.rotation);
     }
 }
